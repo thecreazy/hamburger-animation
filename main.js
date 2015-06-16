6fee8d53a78a8c590a4a8f5f9f35d989da1b2939
The creazy <canella_riccardo@icloud.com>
2015-06-16 17:14:53 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+  $('document').ready(function () {
+      var trigger = $('#hamburger'),
+          isClosed = false;
+
+      trigger.click(function () {
+          burgerTime();
+      });
+
+      function burgerTime() {
+          if (isClosed == true) {
+              trigger.removeClass('is-open');
+              trigger.addClass('is-closed');
+              isClosed = false;
+          } else {
+              trigger.removeClass('is-closed');
+              trigger.addClass('is-open');
+              isClosed = true;
+          }
+      }
+  });
